17f8efdfcc07a92a422d7f98db0dbf229ee46acf
djfor5 <117873331+djfor5@users.noreply.github.com>
2023-10-05 15:36:34 +1100

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif|webp|ico)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext][query]",
+        },
       },
       {
         test: /\.(?:js|mjs|cjs)$/,
@@ -43,6 +46,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Caching",
       template: "./src/template.html",
+      favicon: "./src/assets/favicon.svg",
     }),
   ],
 };
